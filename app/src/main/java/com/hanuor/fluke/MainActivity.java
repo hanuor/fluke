5d067e90ce988dee857130c364b0f6711e555841
hanuor <hanuortech@gmail.com>
2016-04-22 17:39:50 +0530

    Image Uploading finished

--- app/src/main/java/com/hanuor/fluke/MainActivity.java
+++ app/src/main/java/com/hanuor/fluke/MainActivity.java
@@ -1,9 +1,13 @@
 package com.hanuor.fluke;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -25,25 +29,20 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.kinvey.android.Client;
 import com.kinvey.android.callback.KinveyPingCallback;
-import com.kinvey.android.callback.KinveyUserCallback;
 import com.kinvey.android.callback.KinveyUserDeleteCallback;
-import com.kinvey.java.User;
-import com.kinvey.java.core.DownloaderProgressListener;
-import com.kinvey.java.core.MediaHttpDownloader;
-import com.kinvey.java.core.MediaHttpUploader;
-import com.kinvey.java.core.UploaderProgressListener;
-import com.kinvey.java.model.FileMetaData;
 import com.shephertz.app42.paas.sdk.android.App42API;
 import com.shephertz.app42.paas.sdk.android.App42CallBack;
+import com.shephertz.app42.paas.sdk.android.upload.UploadFileType;
+import com.shephertz.app42.paas.sdk.android.upload.UploadService;
 import com.shephertz.app42.paas.sdk.android.user.UserService;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 , this.getApplicationContext()).build();
         printKeyHash();
         final UserService us = App42API.buildUserService();
+        final UploadService upservice = App42API.buildUploadService();
 
         mKinveyClient.ping(new KinveyPingCallback() {
             @Override
@@ -148,122 +148,65 @@ public class MainActivity extends AppCompatActivity {
                         new GraphRequest.GraphJSONObjectCallback() {
                             @Override
                             public void onCompleted(final JSONObject object, GraphResponse response) {
+
                                 try {
-
-                                    getKinveyService.user().create(userid, "ds", new KinveyUserCallback() {
-                                        @Override
-                                        public void onSuccess(User user) {
-
-                                            try {
-                                                getKinveyService.user().put("E_mail",""+object.getString("email"));
-                                                getKinveyService.user().put("Fullname",""+object.getString("name"));
-
-                                                getKinveyService.user().update(new KinveyUserCallback() {
-                                                    @Override
-                                                    public void onSuccess(User user) {
-                                                        Toast.makeText(MainActivity.this, "User Saved into the database success!", Toast.LENGTH_SHORT).show();
-                                                    }
-
-                                                    @Override
-                                                    public void onFailure(Throwable throwable) {
-                                                        Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                });
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-
-                                        @Override
-                                        public void onFailure(Throwable throwable) {
-                                            Toast.makeText(MainActivity.this, ""+throwable, Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                      tv.setText("Hi, " + object.getString("name")+" , "+object.optString("id"));
-                                    final String uid = object.optString("id");
-                                    userid = object.optString("id");
-                                    Picasso.with(MainActivity.this)
-                                            .load("https://graph.facebook.com/" + uid+ "/picture?type=large")
-                                            .into(iv);
-                                    Thread thread = new Thread(new Runnable()
-                                    {
-                                        @Override
-                                        public void run()
-                                        {
-                                            try {
-                                                Log.v("tue","huhi");
-                                               // fin = new FileInputStream(new File("https://graph.facebook.com/" + uid + "/picture?type=large"));
-                                                Log.v("tue2",""+fin);
-                                                is = new URL("https://graph.facebook.com/" + uid + "/picture?type=large").openStream();
-                                                Log.v("tue1",""+is);
-                                                final FileMetaData fmd = new FileMetaData("vamos");
-                                                Log.v("Turn",""+fmd+"here");
-                                                fmd.setPublic(true);
-
-                                                fmd.setFileName(""+userid+"pic");
-                                                getKinveyService.file().upload(fmd, is, new UploaderProgressListener() {
-                                                    @Override
-                                                    public void progressChanged(MediaHttpUploader mediaHttpUploader) throws IOException {
-
-                                                    }
-
-                                                    @Override
-                                                    public void onSuccess(FileMetaData fileMetaData) {
-                                                        Toast.makeText(MainActivity.this, "SAved the profile pic"+fileMetaData, Toast.LENGTH_SHORT).show();
-                                                       Log.v("URL","sdsd");
-                                                        String urloffile =
-                                                                fileMetaData.getDownloadURL();
-                                                        Log.v("URLmm",""+urloffile);
-                                                        Toast.makeText(MainActivity.this, "URL "+" "+urloffile, Toast.LENGTH_SHORT).show();
-                                                        try {
-                                                            final FileOutputStream os = new FileOutputStream(new File(getFilesDir(),"vamosi"));
-                                                            FileMetaData jsd = new FileMetaData("vamos");
-                                                            getKinveyService.file().download(jsd, os, new DownloaderProgressListener() {
-                                                                @Override
-                                                                public void progressChanged(MediaHttpDownloader mediaHttpDownloader) throws IOException {
-
-                                                                }
-
-                                                                @Override
-                                                                public void onSuccess(Void aVoid) {
-                                                                    Toast.makeText(MainActivity.this, "Image Downloaded", Toast.LENGTH_SHORT).show();
-                                                                    Log.v("Os",""+os);
-                                                                }
-
-                                                                @Override
-                                                                public void onFailure(Throwable throwable) {
-                                                                    Log.v("throwable",""+throwable);
-                                                                    Toast.makeText(MainActivity.this, "Error "+throwable, Toast.LENGTH_LONG).show();
-                                                                }
-                                                            });
-                                                        } catch (FileNotFoundException e) {
-                                                            e.printStackTrace();
-                                                        }
-
-
-                                                    }
-
-                                                    @Override
-                                                    public void onFailure(Throwable throwable) {
-                                                        Log.v("Error",""+throwable);
-                                                        Toast.makeText(MainActivity.this, "Error"+throwable, Toast.LENGTH_SHORT).show();
-                                                    }
-                                                });
-                                                //Your code goes here
-                                            }
-                                            catch (Exception e)
-                                            {
-
-                                                e.printStackTrace();
-                                            }
-                                        }
-                                    });
-                                        thread.start();
-
-                                } catch(JSONException ex) {
-                                    ex.printStackTrace();
-
+                                    tv.setText("Hi, " + object.getString("name")+" , "+object.optString("id"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
+                                final String uid = object.optString("id");
+                                userid = object.optString("id");
+                                Picasso.with(MainActivity.this)
+                                        .load("https://graph.facebook.com/" + uid+ "/picture?type=large")
+                                        .into(iv);
+                                Thread thread = new Thread(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        try {
+                                            is = new URL("https://graph.facebook.com/" + uid + "/picture?type=large").openStream();
+                                            upservice.uploadFile("abc.png", is, UploadFileType.IMAGE, "balsh", new App42CallBack() {
+                                                @Override
+                                                public void onSuccess(Object o) {
+                                                  //  Toast.makeText(MainActivity.this, "Boom!", Toast.LENGTH_SHORT).show();
+                                                }
+
+                                                @Override
+                                                public void onException(Exception e) {
+                                                    Log.d("LOL",""+e);
+                                                   // Toast.makeText(MainActivity.this, ""+e, Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        String imageURL = "https://graph.facebook.com/" + uid + "/picture?type=large";
+
+                                        //imageDownload(MainActivity.this,imageURL,uid);
+
+                                    }
+                                });
+                                thread.start();
+                                upservice.uploadFile("abc", is, UploadFileType.IMAGE, "balsh", new App42CallBack() {
+                                    @Override
+                                    public void onSuccess(Object o) {
+                                        Toast.makeText(MainActivity.this, "Boom!", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onException(Exception e) {
+
+                                    }
+                                });
+                                String imageURL = "https://graph.facebook.com/" + uid + "/picture?type=large";
+
+                                Picasso.with(MainActivity.this)
+                                        .load("https://graph.facebook.com/" + uid + "/picture?type=large")
+                                        .into(getTarget(uid));
+                                imageDownload(MainActivity.this,imageURL,uid);
+
+
                             }
                         });
                 Bundle parameters = new Bundle();
@@ -312,6 +255,65 @@ public class MainActivity extends AppCompatActivity {
         if(mp!=null){
             Log.d("blaq",""+mp.getName());
         }
+    }
+
+    public static void imageDownload(Context ctx, String url,String uid){
+        Picasso.with(ctx)
+                .load("https://graph.facebook.com/" + uid + "/picture?type=large")
+                    .into(getTarget("https://graph.facebook.com/" + uid + "/picture?type=large"));
+    }
+
+    //target to save
+    private static Target getTarget(final String url){
+        Target target = new Target(){
+
+            @Override
+            public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        //Bitmap bmp = createBitmap();
+                            Log.v("Hello","Hey");
+                        File stream = new File(Environment.getExternalStorageDirectory().getPath() + "/image1.jpg");
+                        //         + "/you.png");
+                        // stream = new FileOutputStream("/sdcard/you.png");
+                        Log.v("Stream",""+stream);
+                        try {
+                            stream.createNewFile();
+                            FileOutputStream ostream = new FileOutputStream(stream);
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, ostream);
+                            ostream.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } File file = new File(Environment.getExternalStorageDirectory().getPath() + "/" + url);
+                        try {
+                            file.createNewFile();
+                            FileOutputStream ostream = new FileOutputStream(file);
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, ostream);
+                            ostream.flush();
+                            ostream.close();
+
+                        } catch (IOException e) {
+                            Log.e("IOException", e.getLocalizedMessage());
+                        }
+                    }
+                }).start();
+
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+        return target;
     }
 
     private void printKeyHash() {
