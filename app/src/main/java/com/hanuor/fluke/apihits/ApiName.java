3fc412a9574d764af1caa958977519d4decf96c0
shanjohri <shanjohri@gmail.com>
2016-05-13 18:39:23 +0530

    Backing up

--- app/src/main/java/com/hanuor/fluke/apihits/ApiName.java
+++ app/src/main/java/com/hanuor/fluke/apihits/ApiName.java
@@ -8,5 +8,5 @@ public class ApiName {
     public  static final String MUSICGRAPH_ARTIST = "http://api.musicgraph.com/api/v2/artist/search?api_key=527a48eb2415c79cfd66ab7bc66a3904&name=" ;
     public  static final String LASTFM_ARTIST = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=";
     public static  final String LASTFM_APIFORMAT = "&api_key=eb31eea9cedd2291f38f01aec998bcb4&format=json";
-
+    public static final String LASTFM_TRACKINFO = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=eb31eea9cedd2291f38f01aec998bcb4&artist=";
 }
